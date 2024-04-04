6c71bc7e9e59503283ce55489da03d718360492b
EmperorNao <ansicpp2020@gmail.com>
2024-04-04 19:03:38 +0300

    Add Dockerfile, scripts, dependecies, ros nodes for depth to pointcloud with mock depth node

---
+++ rosslam/d.py
@@ -0,0 +1,40 @@
+import argparse
+import subprocess
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='Docker runner')
+
+    parser.add_argument('-b', dest='actions', action='append_const', const="build")
+    parser.add_argument('-s', dest='actions', action='append_const', const="start")
+    parser.add_argument('--image', action='store', type=str, default='rosslam-image')
+    parser.add_argument('--hostname', action='store', type=str, default='rosslam')
+    parser.add_argument('--name', action='store', type=str, default='rosslam-docker-instance')
+
+    args = parser.parse_args()
+
+    # commands example
+    # sudo docker build -t my-image-name .
+    # sudo docker run --hostname rosslam -it --rm --net=host -v .:/rosslam --name my-container-name my-image-name
+    if "build" in args.actions:
+        cmd = ["docker", "build",
+               "-t", args.image,
+               "."
+        ]
+        print(" ".join(cmd))
+        subprocess.check_call(cmd)
+    if "start" in args.actions:
+        cmd = ["docker", "run",
+               "--hostname", args.hostname,
+               "-it", "--rm",
+               "--net=host",
+               "-v", ".:/rosslam",
+               "--name", args.name,
+               args.image
+        ]
+        print(" ".join(cmd))
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as exception:
+            print(exception)
+
