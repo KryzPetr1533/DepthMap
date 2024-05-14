63d19b7c35577e993cd1c7fbf4b6760298243510
devExplorer <petrassassin03@gmail.com>
2024-05-14 03:12:42 +0300

    prepared disparity and camera nodes for merging

--- rosslam/depth_projector/camera_setup/camera_pub.py
+++ rosslam/depth_projector/camera_setup/camera_pub.py
@@ -1,32 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import rclpy                     
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-class ImagePublisher(Node):
 
+class ImagePublisher(Node):
     def __init__(self, name):
         super().__init__(name)
-        self.publisher_ = self.create_publisher(Image, 'image_raw_1', 10)
+        self.left_image = self.create_publisher(Image, 'left_image', 10)
+        self.right_image = self.create_publisher(Image, 'right_image', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
-        self.cap = cv2.VideoCapture(0)
+        self.cap_left = cv2.VideoCapture(0)
+        self.cap_right = cv2.VideoCapture(1)
         self.cv_bridge = CvBridge()
 
     def timer_callback(self):
-        ret, frame = self.cap.read()
+        ret_left, frame_left = self.cap_left.read()
+        ret_right, frame_right = self.cap_right.read()
+
+        if ret_left:
+            self.left_image.publish(
+                self.cv_bridge.cv2_to_imgmsg(frame_left, 'bgr8'))
+        else:
+            self.get_logger().info('No left frame')
         
-        if ret == True:
-            self.publisher_.publish(
-                self.cv_bridge.cv2_to_imgmsg(frame, 'bgr8'))
+        if ret_right:
+            self.right_image.publish(
+                self.cv_bridge.cv2_to_imgmsg(frame_right, 'bgr8'))
+        else:
+            self.get_logger().info('No right frame')
 
         self.get_logger().info('Publishing video frame')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = ImagePublisher("camera_pub_1")
+    node = ImagePublisher("camera_pub")
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
--- rosslam/depth_projector/disparity_estimator/disparity_publisher.py
+++ rosslam/depth_projector/disparity_estimator/disparity_publisher.py
@@ -1,63 +1,93 @@
 import rclpy
 from rclpy.node import Node
 import cv2
-import torch
 import numpy as np
-from cv_bridge import CvBridge
+from msgs_definitions.msg import DisparityData
+import message_filters
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-import os
-
-from your_package_name.msg import DisparityData  # Import your custom message
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
 
 class DisparityPublisher(Node):
     def __init__(self):
         super().__init__('stereo_image_processor')
-        self.publisher_ = self.create_publisher(DisparityData, 'disparity_data', 10)
+        # Load TRT engine
+        self.engine = self.load_trt_engine('some_trt_engine_path')
+        self.context = self.engine.create_execution_context()
+
+        # Basic setup
         self.bridge = CvBridge()
-        self.left_images_dir = '/path/to/left/images'
-        self.right_images_dir = '/path/to/right/images'
         self.timer = self.create_timer(0.5, self.timer_callback)  # Adjust timer as needed
-        self.left_image_files = sorted(os.listdir(self.left_images_dir))
-        self.right_image_files = sorted(os.listdir(self.right_images_dir))
-        self.index = 0
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = self.load_model().to(self.device)
-        self.model.eval()
 
-    def load_model(self):
-        model_path = 'path_to_your_model.pth'
-        model = torch.load(model_path, map_location=self.device)
-        return model
-
-    def timer_callback(self):
-        if self.index >= len(self.left_image_files) or self.index >= len(self.right_image_files):
-            self.index = 0  # Reset or stop
+        # Images subscription
+        self.left_image_sub = message_filters.Subscriber(self, 'left_image', Image)
+        self.right_image_sub = message_filters.Subscriber(self, 'right_image', Image)
+        self.get_logger().info('Synchronizing images')
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.left_image_sub, self.right_image_sub], 10, 0.1, allow_headless=True)
+        self.ts.registerCallback(self.image_callback)
+        # Disparity publisher
+        self.disparity_pub = self.create_publisher(DisparityData, 'disparity_data', 10)
+    
+    def load_trt_engine(self, engine_path):
+        '''
+        Load TRT engine
+        '''
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+        with open(engine_path, 'rb') as f, trt.Runtime(TRT_LOGGER) as runtime:
+            engine = runtime.deserialize_cuda_engine(f.read())
+        return engine
+    
+    def image_callback(self, left_msg, right_msg):
+        '''
+        Convert messages to images RGB (important)
+        Run disparity function
+        Publish disparity
+        '''
+        try:
+            left_img = self.bridge.imgmsg_to_cv2(left_msg, desired_encoding='rgb8') # Important to use rgb8 for TRT
+            right_img = self.bridge.imgmsg_to_cv2(right_msg, desired_encoding='rgb8')
+        except CvBridgeError as e:
+            self.get_logger().info('Failed to convert image: {}'.format(e))
             return
-
-        left_img_path = os.path.join(self.left_images_dir, self.left_image_files[self.index])
-        right_img_path = os.path.join(self.right_images_dir, self.right_image_files[self.index])
-
-        left_img = cv2.imread(left_img_path, cv2.IMREAD_GRAYSCALE)
-        right_img = cv2.imread(right_img_path, cv2.IMREAD_GRAYSCALE)
-
+        
         disparity_data = self.process_images(left_img, right_img)
-
         msg = DisparityData()
         msg.height = disparity_data.shape[0]
         msg.width = disparity_data.shape[1]
-        msg.data = disparity_data.flatten().tolist()
-        self.publisher_.publish(msg)
-        self.index += 1
+        msg.matr = disparity_data.flatten().tolist()
+        self.disparity_pub.publish(msg)
 
     def process_images(self, left_img, right_img):
         '''
+        Convert images to CHW format
+        Run inference
+        Return disparity
         '''
-        left_tensor = torch.tensor(left_img).unsqueeze(0).unsqueeze(0).float().to(self.device)
-        right_tensor = torch.tensor(right_img).unsqueeze(0).unsqueeze(0).float().to(self.device)
+        # Convert to CHW format for TRT
+        left_img_chw = left_img.transpose((2, 0, 1))
+        right_img_chw = right_img.transpose((2, 0, 1))
 
-        with torch.no_grad():
-            disparity = self.model(left_tensor, right_tensor).squeeze().cpu().numpy()
-        return disparity
+        d_left_img = cuda.mem_alloc(left_img_chw.nbytes)
+        d_right_img = cuda.mem_alloc(right_img_chw.nbytes)
+        d_disparity = cuda.mem_alloc(left_img_chw.nbytes)
+        bindings = [int(d_left_img), int(d_right_img), int(d_disparity)]
+
+        stream = cuda.Stream()
+        # Copy images to the GPU
+        cuda.memcpy_htod_async(d_left_img, left_img_chw, stream)
+        cuda.memcpy_htod_async(d_right_img, right_img_chw, stream)
+
+        # Run inference
+        self.context.execute_async(bindings=bindings, stream_handle=stream.handle)
+
+        # Copy result from GPU
+        output = np.empty((right_img_chw.shape[1], right_img_chw.shape[2]), dtype=np.int16)
+        cuda.memcpy_dtoh_async(output, d_disparity, stream)
+        stream.synchronize()
+
+        return output
 
 def main(args=None):
     rclpy.init(args=args)
