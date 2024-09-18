3a9613ab01505b770d7a5804790c7afbab9051b2
KryzPetr1533 <petrassassin@gmail.com>
2024-09-18 19:05:58 +0300

    new dirs architecture

--- rosslam/b.sh
+++
@@ -1,5 +0,0 @@
-# currently we build all packages
-rosdep install -i --from-path . --rosdistro humble -y
-colcon build
-set -a
-source install/setup.bash
--- rosslam/launch.sh
+++
@@ -1,2 +0,0 @@
-source b.sh
-ros2 launch ./depth_projector/launch/depth.launch frame_width:=640 frame_height:=480
